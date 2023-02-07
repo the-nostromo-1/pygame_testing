4710563b4ab1fc3692792856b5da75dde3d6237e
the-nostromo-1 <tinman11537@gmail.com>
2023-02-07 08:15:31 -0500

    Refactoring

--- pygame_yt_intro_v2.py
+++ pygame_yt_intro_v2.py
@@ -19,25 +19,27 @@ screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGTH))
 clock = pygame.time.Clock()
 
 scoreFont = pygame.font.Font(
-    "/Users/djt/Desktop/pygame_testing/font_assets/Pixeltype.ttf", 50)
+    "/Users/djt/Desktop/pyGameTesting/font_assets/Pixeltype.ttf", 50)
 scoreSurface = scoreFont.render('My Game', False, (254, 128, 25))
 scoreRect = scoreSurface.get_rect(center=(SCREEN_WIDTH/2, 50))
 
 background = pygame.image.load(
-    "/Users/djt/Desktop/pygame_testing/image_assets/sky.png").convert_alpha()
+    "/Users/djt/Desktop/pyGameTesting/image_assets/sky.png").convert_alpha()
 background = pygame.transform.scale(background, (SCREEN_WIDTH, SCREEN_HEIGTH))
 
 grass = pygame.image.load(
-    "/Users/djt/Desktop/pygame_testing/image_assets/ground.png")
+    "/Users/djt/Desktop/pyGameTesting/image_assets/ground.png")
 grass = pygame.transform.scale(grass, (SCREEN_WIDTH, 200))
 
 snail = pygame.image.load(
-    "/Users/djt/Desktop/pygame_testing/image_assets/snail/snail1.png").convert_alpha()
+    "/Users/djt/Desktop/pyGameTesting/image_assets/snail/snail1.png")\
+    .convert_alpha()
 snailRect = snail.get_rect(bottomright=(SCREEN_WIDTH - 100, 325))
 snailXPos = 700
 
 player = pygame.image.load(
-    "/Users/djt/Desktop/pygame_testing/image_assets/Player/player_walk_1.png").convert_alpha()
+    "/Users/djt/Desktop/pyGameTesting/image_assets/Player/player_walk_1.png")\
+    .convert_alpha()
 playerRect = player.get_rect(midbottom=(SCREEN_WIDTH/3, 325))
 
 while True:
@@ -72,8 +74,8 @@ while True:
     # mousePos = pygame.mouse.get_pos()
     # checking if mouse position is colliding with the player
     # if playerRect.collidepoint(mousePos):
-    #     # returns a bool of true or false if lmb, middle button, or rmb is pressed
-    #     print(pygame.mouse.get_pressed())
+    # returns a bool of true or false if lmb, middle button, or rmb is pressed
+    # print(pygame.mouse.get_pressed())
 
     pygame.display.update()
     clock.tick(FRAME_RATE)
